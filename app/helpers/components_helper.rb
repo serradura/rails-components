a8e8f392916fd997f63805bf9c7f71fcecd78bd0
Rodrigo Serradura <rodrigo@ysimplicity.com>
2018-03-01 00:28:33 -0300

    Create component helper

---
+++ app/helpers/components_helper.rb
@@ -0,0 +1,25 @@
+module ComponentsHelper
+  def component(name, locals=nil, &block)
+    render_component(
+      component_path(controller_path, name),
+      locals,
+      &block
+    )
+  end
+
+  def render_component(name, locals={}, &block)
+    if block_given?
+      # using `layout` is a trick to allow passing blocks to partials
+      # (cf. http://stackoverflow.com/a/2952056)
+      render layout: name, locals: locals, &block
+    else
+      render partial: name, locals: locals
+    end
+  end
+
+  private
+
+  def component_path(namespace, name)
+    "#{namespace}/components/#{name}/show"
+  end
+end
